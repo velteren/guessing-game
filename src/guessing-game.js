5185ab83c930bf6fde1994b30c18312af44949de
Konstantin <77935386+velteren@users.noreply.github.com>
2022-08-11 15:03:16 +0300

    feat: add final solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.currentTry = Math.round(this.min + (this.max - this.min)/2);
+        console.log("Current Try: " + this.currentTry);
+        return(this.currentTry);
     }
 
     lower() {
-
+        this.max = this.currentTry;
     }
 
     greater() {
-
+        this.min = this.currentTry;
     }
 }
 
